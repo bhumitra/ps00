025737f0e986c95db2959735cd1acc3edfc3dffe
bhumitra nagar <nagar.b@husky.neu.edu>
2014-09-06 14:43:23 -0400

    ex 5,6,7,8

---
+++ Ex 5.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Ex 5|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+; sq : Number -> Number
+; GIVEN: A number is passed as an argument
+; RETURNS: the square of the number 
+; Examples:
+; (sq 5)  => 25
+; (sq 11) => 121
+; (sq -4) => 16
+
+(define (sq x)
+    (* x x))
+
+(sq 5)
+(sq 11)
+(sq -4)
---
+++ Ex 8.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |Ex 8|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+(* pi (expt 1 2))
+(* pi (expt 5 2))
+(* pi (expt 7 2))
+
+; area-circle : Number -> Number
+; GIVEN: the radius r of a circle 
+; RETURNS: its area, using the formula pi * r^2
+; Examples:
+; (area-circle 1)  =>  #i3.141592653589793 
+; (area-circle 5)  =>  #i78.53981633974483
+; (area-circle 7)  =>  #i153.93804002589985
+
+
+
