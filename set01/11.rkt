5a575da348a752789e43439c59bbc921da512505
bhumitra nagar <nagar.b@husky.neu.edu>
2014-09-10 16:50:51 -0400

    comitting ex11

---
+++ set01/11.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname |11|) (read-case-sensitive #t) (teachpacks ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "image.rkt" "teachpack" "2htdp") (lib "universe.rkt" "teachpack" "2htdp")))))
+; make-point : Number Number -> Point
+; takes two numbers as arguments and returns position of point on 2D plane
+
+; point? : Any -> boolean
+; This predicate takes any value as an argument and returns true if 
+; that argument is of the type point
+
+; point-x : Point -> Number
+; takes argument of type Point and returns x component of it
+
+; point-y : Point -> Number
+; takes argument of type Point and returns y component of it
+
